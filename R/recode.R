540d160a3552e4477a229469009ad25f9de17579
dgromer <dgromer@mailbox.org>
2015-09-24 00:20:07 +0200

    Improve recode documentation

--- R/recode.R
+++ R/recode.R
@@ -1,15 +1,24 @@
 #' Recode variables
-#'
+#' 
 #' @param x numeric or character vector with values to recode.
-#' @param ... recode pairs, e.g. \code{"female" = "f"}.
-#' @param default numeric or character. All entries in \code{x} not covered by
+#' @param ... recode pairs in the format \code{"from" = "to"}, e.g.
+#'   \code{"female" = "f"}.
+#' @param default numeric or character. All entries in \code{x} not covered by 
 #'   \code{...} get recoded to \code{default}.
-#' @param coerce a function that is applied to \code{x} after recoding, e.g.
+#' @param coerce a function that is applied to \code{x} after recoding, e.g. 
 #'   \code{as.numeric}.
+#' @details 
+#' The left hand side of the recode pairs in \code{...} can be one of:
+#' \describe{
+#'   \item{Single value}{e.g. \code{"CTX" = "Context"}}
+#'   \item{Range of values}{e.g. \code{"1:10" = 5}}
+#'   \item{Vector of values}{e.g. \code{"c('A', 'B')" = "AB"}}
+#' }
 #' @examples
 #' recode(c("female", "male", "male", "female"), "female" = "f", "male" = "m")
 #' 
 #' recode(1:10, "1:5" = "a", "6:10" = "b")
+#' recode(1:20, "1:10" = 5, "11:20" = 15)
 #' 
 #' recode(sample(letters[1:6]), "c('a', 'b', 'c')" = "abc",
 #'        "c('d', 'e', 'f')" = "def")
