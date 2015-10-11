c7e442d51cca3a7e5636fa8fa77e87ec66d4058b
dgromer <dgromer@mailbox.org>
2015-10-11 17:20:02 +0200

    Allow specification of statistics in stats_table

--- R/stats_table.R
+++ R/stats_table.R
@@ -1,20 +1,21 @@
 #' Statistical table
-#' 
+#'
 #' Compiles a table with descriptive and inferential statistics to compare group
 #' characteristics, e.g. control variables of a treatment group and a control
 #' group.
-#' 
+#'
 #' @importFrom dplyr select
 #' @importFrom purrr splice
 #' @importFrom htmlTable htmlTable
+#' @importFrom magrittr %<>%
 #' @importFrom rmarkdown pandoc_convert
 #' @param .data a data frame
 #' @param iv name of the independent variable
 #' @param ... names of the dependent variables
 #' @param sig logical indicating whether to show significance with symbols in
 #'   the last column
-#' @param format character string indicating the output format, one of 
-#'   \code{"default"}, \code{"html"} or \code{"latex"}. For HTML, the table is 
+#' @param format character string indicating the output format, one of
+#'   \code{"default"}, \code{"html"} or \code{"latex"}. For HTML, the table is
 #'   displayed as a htmlwidget. For LaTeX, the \code{tabularx} environment is
 #'   used, which requires the tabularx package. The utilized \code{\\cmidrule}
 #'   command requires either the booktabs or ctable package.
@@ -26,46 +27,58 @@
 #' stats_table(height, iv = group, age:aq_avoi, sig = TRUE, format = "html",
 #'             args = list(var.equal = TRUE))
 #' @export
-stats_table <- function(.data, iv, ..., sig = FALSE,
+stats_table <- function(.data, iv, ..., funs = c("mean", "sd"), sig = FALSE,
                         format = c("default", "html", "latex"), args = list())
 {
   format <- match.arg(format)
-  
+
   # Convert iv to character if it is a name
   if (!is.character(substitute(iv)))
   {
     iv <- as.character(substitute(iv))
   }
-  
+
   # Get a vector of the levels of the iv
   group_names <- as.character(unique(.data[[iv]]))
 
   # Get the column names of the dvs
   dvs <- names(select(.data, ...))
-  
-  header <- stats_table_header(group_names, sig)
-  
+
+  # TODO: replace funs names with M, SD, etc.
+  header <- stats_table_header(group_names, funs, sig)
+
   # Calculate statistics for each dependent variable
   if (length(group_names) == 2)
   {
     rows <- lapply(dvs, function(dv)
       do.call("stats_table_row_ttest", splice(.data = .data, dv = dv, iv = iv,
-                                              sig = sig, args)))
+                                              funs = funs, sig = sig, args)))
   }
   else
   {
-    rows <- lapply(dvs, function(dv) stats_table_row_aov(.data, dv, iv, sig))
+    rows <- lapply(dvs, function(dv) stats_table_row_aov(.data, dv, iv, funs,
+                                                         sig))
   }
-  
+
   rows <- lapply(rows, matrix, nrow = 1)
   tbl <- as.data.frame(Reduce(rbind, rows), stringsAsFactors = FALSE)
   names(tbl) <- header
   row.names(tbl) <- NULL
-  
+
+  # All statistics get formatted with two decimal places. If n was requested,
+  # reformat this with no decimal places
+  if ("n" %in% funs)
+  {
+    tbl[, grep("^n_", names(tbl))] %<>% lapply(function(.) round(as.numeric(.)))
+  }
+
   if (format == "html")
   {
+    # Remove group names from statistics, e.g. "mean_group" to "mean", since
+    # group names go to their own lines
     header <- gsub("_.*$", "", names(tbl))[-1]
-    
+
+    # Use htmlTable::htmlTable
     htmlTable(
       tbl[, -1],
       header = header,
@@ -78,7 +91,7 @@ stats_table <- function(.data, iv, ..., sig = FALSE,
   else if (format == "latex")
   {
     header <- gsub("_.*$", "", names(tbl))[-1]
-    
+
     cat("\\begin{tabularx}{\\textwidth}{Xrrrrrrr}\n\\hline\n",
         paste0(" & \\multicolumn{2}{c}{", group_names, "}"), " & & & \\\\\n",
         "\\cmidrule(lr){2-3}\\cmidrule(lr){4-5}\n",
@@ -97,77 +110,61 @@ stats_table <- function(.data, iv, ..., sig = FALSE,
 tbl_to_latex <- function(x)
 {
   x %>%
+    # Separate columns with &
     apply(1, paste, collapse = " & ") %>%
+    # Separate rows with \\ and line break
     paste(collapse = " \\\\\n")
 }
 
-stats_table_header <- function(iv, sig)
+stats_table_header <- function(iv, funs, sig)
 {
   sig <- if (sig) "sig" else character(0)
-  
-  c("Variable", paste(c("M", "SD"), rep(iv, each = 2), sep = "_"),
+
+  c("Variable", paste(funs, rep(iv, each = length(funs)), sep = "_"),
     ifelse(length(iv) > 2, "F", "t"), "p",
     ifelse(length(iv) > 2, "petasq", "d"), sig)
 }
 
 
-stats_table_row_aov <- function(.data, dv, iv, es = "petasq", sig)
+stats_table_row_aov <- function(.data, dv, iv, es = "petasq", funs, sig)
 {
-  stats <- stats_table_descriptives(.data, dv, iv)
-  
+  stats <- stats_table_descriptives(.data, dv, iv, funs)
+
   test <- anova(aov(as.formula(paste(dv, "~", iv)), .data))
 
-   f <- fmt_stat(test[["F value"]][1])
-   p <- fmt_pval(test[["Pr(>F)"]][1], equal_sign = FALSE)
-   petasq <- fmt_es(test[["Sum Sq"]][1] / sum(test[["Sum Sq"]]),
-                    equal_sign = FALSE)
-   sig <- if (sig)  p_to_symbol(test[["Pr(>F)"]][1]) else character(0)
-  
+  f <- test[["F value"]][1] %>% fmt_stat(equal_sign = FALSE)
+  p <- test[["Pr(>F)"]][1] %>% fmt_pval(equal_sign = FALSE)
+  petasq <- test[["Sum Sq"]][1] / sum(test[["Sum Sq"]]) %>%
+    fmt_es(equal_sign = FALSE)
+  sig <- if (sig)  test[["Pr(>F)"]][1] %>% p_to_symbol() else character(0)
+
   c(dv, stats, f, p, petasq, sig)
 }
 
-stats_table_row_ttest <- function(.data, dv, iv, sig, ...)
+#' @importFrom magrittr %>%
+stats_table_row_ttest <- function(.data, dv, iv, funs, sig, ...)
 {
-  stats <- stats_table_descriptives(.data, dv, iv)
-  
+  stats <- stats_table_descriptives(.data, dv, iv, funs)
+
   test <- t_test(as.formula(paste(dv, iv, sep = " ~ ")), .data, ...)
-  
-  t <- fmt_stat(test$statistic)
-  p <- fmt_pval(test$p.value, equal_sign = FALSE)
-  d <- fmt_es(cohens_d(test), equal_sign = FALSE)
-  sig <- if (sig) p_to_symbol(test$p.value) else character(0)
-  
+
+  t <- test$statistic %>% fmt_stat(equal_sign = FALSE)
+  p <- test$p.value %>% fmt_pval(equal_sign = FALSE)
+  d <- cohens_d(test) %>% fmt_es(equal_sign = FALSE)
+  sig <- if (sig) test$p.value %>% p_to_symbol() else character(0)
+
   c(dv, stats, t, p, d, sig)
 }
 
-stats_table_descriptives <- function(.data, dv, iv)
-{  
-  m <- sapply(split(.data[[dv]], .data[[iv]]), mean, na.rm = TRUE)
-  sd <- sapply(split(.data[[dv]], .data[[iv]]), sd, na.rm = TRUE)
-  
-  fmt_stat(c(rbind(m, sd)))
-}
-
-p_to_symbol <- function(p)
+#' @importFrom magrittr %>%
+stats_table_descriptives <- function(.data, dv, iv, funs)
 {
-  if (p >= .1)
-  {
-    ""
-  }
-  else if (p < .1 && p >= .05)
-  {
-    "."
-  }
-  else if (p < .05 && p >= .01)
-  {
-    "*"
-  }
-  else if (p < .01 && p >= .001)
-  {
-    "**"
-  }
-  else if (p < .001)
-  {
-    "***"
-  }
+  # Calculate descriptive statistics
+  ds(.data, as.formula(paste(dv, iv, sep = " ~ ")), funs, na.rm = TRUE) %>%
+    # Extract the statistics from the data frame
+    (function(.x) .x[, funs]) %>%
+    # Bring them into the correct order
+    (function(.x) c(t(as.matrix(.x)))) %>%
+    # Format values
+    sapply(fmt_stat, equal_sign = FALSE)
 }
