56ae4a1eb3d07a60defb23d08b7b4390da26d15c
dgromer <dgromer@mailbox.org>
2015-10-25 18:14:18 +0100

    Functions of confidence interval and margin of error

---
+++ R/ci.R
@@ -0,0 +1,29 @@
+#' Margin of Error
+#'
+#' @param x a numeric vector or an \R object which is coercible to one by
+#'   \code{as.vector(x, "numeric")}
+#' @param quantile
+#' @param na.rm logical. Should missing values be removed?
+#' @export
+moe <- function(x, quantile = .975, na.rm = FALSE)
+{
+  # Calculate degrees of freedom as n - 1
+  df <- if(!na.rm) length(x) - 1 else length(na.omit(x)) - 1
+
+  se(x, na.rm) * qt(quantile, df)
+}
+
+#' Confidence Interval
+#'
+#' @param x a numeric vector or an \R object which is coercible to one by
+#'   \code{as.vector(x, "numeric")}
+#' @param quantile
+#' @param na.rm logical. Should missing values be removed?
+#' @export
+ci <- function(x, quantile = .975, na.rm = FALSE)
+{
+  m <- mean(x, na.rm = na.rm)
+  moe <- moe(x, quantile, na.rm)
+
+  list(lower = m - moe, upper = m + moe)
+}
