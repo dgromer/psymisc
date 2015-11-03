7c066222c0d728194cbc90e10a72060a7bf66da0
dgromer <dgromer@mailbox.org>
2015-11-03 21:11:31 +0100

    Support for different type error bars in fplot

--- R/fplot.R
+++ R/fplot.R
@@ -1,23 +1,45 @@
 #' fplot
 #'
-#' Convenient plotting of means and standard errors or boxplots of factorial
+#' Convenient plotting of means and errors bars or boxplots of factorial
 #' data.
 #'
-#' @importFrom dplyr left_join
-#' @importFrom magrittr %<>%
+#' @importFrom dplyr left_join rename_
+#' @importFrom magrittr %<>% %>%
 #' @importFrom tidyr complete_
 #' @param .data a data frame containing the variables in the formula
 #'   \code{formula}.
-#' @param formula a formula in the form \code{lhs ~ rhs} where \code{lhs} is
-#'   the a numeric variable (dependent variable) and \code{rhs} one or more
+#' @param formula a formula in the form \code{lhs ~ rhs} where \code{lhs} is a
+#'   numeric variable (the dependent variable) and \code{rhs} one or more
 #'   factors with two or more levels giving the corresponding groups.
-#' @param geom character indicating the ggplot2 geom used. One of \code{"bar"}
-#'   (default), \code{"line"} and \code{"boxplot"}.
+#' @param geom character string indicating the ggplot2 geom used. One of
+#'   \code{"bar"} (default), \code{"line"} and \code{"boxplot"}.
+#' @param error character string indicating the function used for calculating
+#'   the error bars. One of \code{"se"} (default) \code{"sd"} or \code{"moe"}.
+#'   Ignored if \code{geom} is \code{"boxplot"}.
+#' @param ... Further arguments passed to methods. E.g. \code{quantile} if
+#'   \code{error} is \code{"moe"} to indicate the quantile for the confidence
+#'   interval.
+#' @examples
+#' fplot(height, stai_trait ~ group + sex)
+#'
+#' # Standard deviation instead of standard error
+#' fplot(height, stai_trait ~ group + sex, error = "sd")
+#'
+#' # 95% confidence intervals (moe = margin of error)
+#' fplot(height, stai_trait ~ group + sex, error = "moe")
+#'
+#' # Line graph
+#' fplot(height, stai_trait ~ group + sex, geom = "line")
+#'
+#' # Boxplot
+#' fplot(height, stai_trait ~ group + sex, geom = "boxplot")
 #'
 #' @export
-fplot <- function(.data, formula, geom = c("bar", "line", "boxplot"))
+fplot <- function(.data, formula, geom = c("bar", "line", "boxplot"),
+                  error = c("se", "sd", "moe"), ...)
 {
   geom <- match.arg(geom)
+  error <- match.arg(error)
 
   # Get dependent variable
   dv <- all.vars(formula)[1]
@@ -30,11 +52,14 @@ fplot <- function(.data, formula, geom = c("bar", "line", "boxplot"))
     return(fplot_boxplot(.data, dv, vars))
   }
 
-  # Calculate means and standard errors
-  descr <- ds(.data, formula, na.rm = TRUE)
-
-  # Keep empty groups in descr
-  descr <- complete_(descr, vars, fill = list())
+  descr <-
+    # Calculate means and error bars
+    ds(.data, formula, c("mean", error), na.rm = TRUE, ...) %>%
+    # Rename column indicating error to "error", since it can be either "se",
+    # "sd" or "moe".
+    rename_(.dots = setNames(error, "error")) %>%
+    # Keep empty groups in descr
+    complete_(vars, fill = list())
 
   # Ensure that independent variables are factors
   descr[1:length(vars)] %<>% lapply(as.factor)
@@ -49,7 +74,7 @@ fplot_bar <- function(x, dv, vars)
   {
     ggplot(x, aes_string(x = vars, y = "mean")) +
       geom_bar(stat = "identity") +
-      geom_errorbar(aes(ymin = mean - se, ymax = mean + se), width = .2) +
+      geom_errorbar(aes(ymin = mean - error, ymax = mean + error), width = .2) +
       theme_classic() +
       labs(y = dv)
   }
@@ -57,7 +82,7 @@ fplot_bar <- function(x, dv, vars)
   {
     ggplot(x, aes_string(x = vars[1], y = "mean", fill = vars[2])) +
       geom_bar(stat = "identity", position = "dodge") +
-      geom_errorbar(aes(ymin = mean - se, ymax = mean + se), width = .2,
+      geom_errorbar(aes(ymin = mean - error, ymax = mean + error), width = .2,
                     position = position_dodge(.9)) +
       theme_classic() +
       labs(y = dv)
@@ -67,7 +92,7 @@ fplot_bar <- function(x, dv, vars)
   {
     ggplot(x, aes_string(x = vars[1], y = "mean", fill = vars[2])) +
       geom_bar(stat = "identity", position = "dodge") +
-      geom_errorbar(aes(ymin = mean - se, ymax = mean + se), width = .2,
+      geom_errorbar(aes(ymin = mean - error, ymax = mean + error), width = .2,
                     position = position_dodge(.9)) +
       facet_grid(as.formula(paste(". ~", vars[3]))) +
       theme_bw() +
@@ -77,7 +102,7 @@ fplot_bar <- function(x, dv, vars)
   {
     ggplot(x, aes_string(x = vars[1], y = "mean", fill = vars[2])) +
       geom_bar(stat = "identity", position = "dodge") +
-      geom_errorbar(aes(ymin = mean - se, ymax = mean + se), width = .2,
+      geom_errorbar(aes(ymin = mean - error, ymax = mean + error), width = .2,
                     position = position_dodge(.9)) +
       facet_grid(as.formula(paste(vars[4], "~", vars[3]))) +
       theme_bw() +
@@ -97,7 +122,7 @@ fplot_line <- function(x, dv, vars)
     ggplot(x, aes_string(x = vars, y = "mean")) +
       geom_point(na.rm = TRUE) +
       geom_line(aes(group = 1), na.rm = TRUE) +
-      geom_errorbar(aes(ymin = mean - se, ymax = mean + se), width = .2,
+      geom_errorbar(aes(ymin = mean - error, ymax = mean + error), width = .2,
                     na.rm = TRUE) +
       theme_classic() +
       labs(y = dv)
@@ -108,7 +133,7 @@ fplot_line <- function(x, dv, vars)
                          color = vars[2])) +
       geom_point(na.rm = TRUE) +
       geom_line(na.rm = TRUE) +
-      geom_errorbar(aes(ymin = mean - se, ymax = mean + se), width = .2,
+      geom_errorbar(aes(ymin = mean - error, ymax = mean + error), width = .2,
                     na.rm = TRUE) +
       theme_classic() +
       labs(y = dv)
@@ -120,7 +145,7 @@ fplot_line <- function(x, dv, vars)
                          color = vars[2])) +
       geom_point(na.rm = TRUE) +
       geom_line(na.rm = TRUE) +
-      geom_errorbar(aes(ymin = mean - se, ymax = mean + se), width = .2,
+      geom_errorbar(aes(ymin = mean - error, ymax = mean + error), width = .2,
                     na.rm = TRUE) +
       facet_grid(as.formula(paste(". ~", vars[3]))) +
       theme_bw() +
@@ -132,7 +157,7 @@ fplot_line <- function(x, dv, vars)
                          color = vars[2])) +
       geom_point(na.rm = TRUE) +
       geom_line(na.rm = TRUE) +
-      geom_errorbar(aes(ymin = mean - se, ymax = mean + se), width = .2,
+      geom_errorbar(aes(ymin = mean - error, ymax = mean + error), width = .2,
                     na.rm = TRUE) +
       facet_grid(as.formula(paste(vars[4], "~", vars[3]))) +
       theme_bw() +
