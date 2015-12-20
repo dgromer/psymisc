e71988c9427bf72564b944c370e6c7afe61c5c65
dgromer <dgromer@mailbox.org>
2015-12-20 23:19:51 +0100

    Support logical operations in recode

--- R/recode.R
+++ R/recode.R
@@ -1,5 +1,6 @@
 #' Recode variables
 #'
+#' @importFrom stringr str_trim
 #' @param x numeric or character vector with values to recode.
 #' @param ... recode pairs in the format \code{"from" = "to"}, e.g.
 #'   \code{"female" = "f"}.
@@ -10,15 +11,21 @@
 #' @details
 #' The left hand side of the recode pairs in \code{...} can be one of:
 #' \describe{
-#'   \item{Single value}{e.g. \code{"CTX" = "Context"}}
-#'   \item{Range of values}{e.g. \code{"1:10" = 5}}
-#'   \item{Vector of values}{e.g. \code{"c('A', 'B')" = "AB"}}
+#'   \item{Single value}{e.g. \code{"CTX" = "Context"}, \code{"3" = 5}}
+#'   \item{Range of values}{e.g. \code{"1:10" = 5}, \code{"6:10" = "above
+#'   five"}}
+#'   \item{Vector of values}{e.g. \code{"c('A', 'B')" = "AB"} (note the use of
+#'   single quotes inside the double quotes)}
+#'   \item{Logical operation}{e.g. \code{"< 20" = "small"}, \code{"<= 10 | > 90"
+#'   = "outlier"}, \code{"> 40 & < 60" = "normal"}}
 #' }
 #' @examples
 #' recode(c("female", "male", "male", "female"), "female" = "f", "male" = "m")
 #'
 #' recode(1:10, "1:5" = "a", "6:10" = "b")
 #' recode(1:20, "1:10" = 5, "11:20" = 15)
+#'
+#' recode(1:20, "< 3" = "small", ">= 3 & <= 8" = "medium", "> 8" = "large")
 #'
 #' recode(sample(letters[1:6]), "c('a', 'b', 'c')" = "abc",
 #'        "c('d', 'e', 'f')" = "def")
@@ -30,24 +37,42 @@ recode <- function(x, ..., default = NULL, coerce = NULL)
   from <- names(r)
   to <- unlist(unname(r))
 
-  # Vector indicating which element in x has already been recoded so a single
+  # Make a copy of 'x' that will be filled with new values so we don't run into
+  # the problem of numeric vector being changed to character vector and logical
+  # operations not working anymore
+  xout <- x
+
+  # Vector indicating which element in 'x' has already been recoded so a single
   # element is not changed twice
   recoded <- logical(length(x))
 
   for (i in seq_along(from))
   {
-    # Check if 'from' is a vector
+    # Find elements to be recoded
+    # 'from' is a vector of values (e.g. c("a", "b") or 1:5)
     if (grepl("^c\\(.+\\)$", from[i]) || grepl("^[0-9]+\\:[0-9]+$", from[i]))
     {
       matches <- x %in% eval(parse(text = from[i]))
     }
+    # 'from' is a logical operation (e.g. > 20 or > 5 & < 20)
+    else if (grepl("^[<>]=?", from[i]))
+    {
+      # Add 'x' to the logical operation
+      s <- paste("x", str_trim(from[i]))
+
+      # Add 'x' a second time after "and" or "or" operator if present
+      if (grepl("[&\\|]", s)) s <- gsub("^(.*[&\\|])(.*)$", "\\1x\\2", s)
+
+      matches <- eval(parse(text = s))
+    }
+    # 'from' is a single value
     else
     {
       matches <- x %in% from[i]
     }
 
     # Recode matches
-    x[matches & !recoded] <- to[i]
+    xout[matches & !recoded] <- to[i]
 
     # Add recoded elements to recoded
     recoded <- recoded | matches
@@ -56,13 +81,13 @@ recode <- function(x, ..., default = NULL, coerce = NULL)
   if (!is.null(default))
   {
     # Set all elements that didn't get recoded so far to 'default'
-    x[!recoded] <- default
+    xout[!recoded] <- default
   }
 
   if (!is.null(coerce))
   {
-    x <- do.call(coerce, list(x))
+    xout <- do.call(coerce, list(xout))
   }
 
-  x
+  xout
 }
--- tests/testthat/test-recode.R
+++ tests/testthat/test-recode.R
@@ -19,6 +19,38 @@ test_that("vector recoding", {
                c(rep("abc", 3), rep("def", 3)))
 })
 
+test_that("logical recoding", {
+
+  # Smaller than
+  expect_equal(recode(1:5, "< 3" = 0), c(rep(0, 2), 3:5))
+  expect_equal(recode(c(.3, 1.5, .8), "< .9" = 0), c(0, 1.5, 0))
+
+  # Smaller than or equal to
+  expect_equal(recode(1:5, "<= 3" = 0), c(rep(0, 3), 4:5))
+  expect_equal(recode(c(.123, .8, .34), "<= .34" = 0), c(0, .8, 0))
+
+  # Greater than
+  expect_equal(recode(1:20, "> 15" = 15), c(1:15, rep(15, 5)))
+  expect_equal(recode(c(.9, .231, .71, .48), "> 0.703" = 1), c(1, .231, 1, .48))
+
+  # Equal to or greater
+  expect_equal(recode(1:5, ">= 4" = 5), c(1:3, rep(5, 2)))
+  expect_equal(recode(c(.8, .3, .5), ">= .5" = 1.2), c(1.2, .3, 1.2))
+
+  # With logical and
+  expect_equal(recode(1:5, "<= 2" = 1, "> 2 & <= 3" = 5, "> 3" = 10),
+               c(rep(1, 2), 5, rep(10, 2)))
+  expect_equal(recode(1:6, "< 3" = "small", ">= 3 & <= 5" = "medium",
+                      "> 5" = "large"),
+               c(rep("small", 2), rep("medium", 3), "large"))
+  expect_equal(recode(c(.3, .7, .1, .6, .9), "> .2 & < .8" = .5),
+               c(rep(.5, 2), .1, .5, .9))
+
+  # With logical or
+  expect_equal(recode(1:5, "< 2 | > 4" = "outlier", default = "normal"),
+               c("outlier", rep("normal", 3), "outlier"))
+})
+
 test_that("recode with default value", {
   expect_equal(recode(c(1, 5, 8, 3), "1" = "one", "3" = "three", "5" = "five",
                       default = "no idea"),
